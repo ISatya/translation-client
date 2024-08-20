441f3b1c271a55fcd0172d52b5a026beb71e2cf2
Satya Iyengar <isatya1992@gmail.com>
2024-08-20 17:43:14 +0530

    integrate google login

--- src/App.js
+++ src/App.js
@@ -2,21 +2,37 @@ import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
 import { Button } from '@mui/material';
+import { GoogleOAuthProvider, GoogleLogin } from '@react-oauth/google';
+
 
 import VideoCall from './components/VideoCall';
 import BroadcastVid from './components/BroadcastVid';
 
 function App() {
   const [mode,setMode] = useState(null);
+   const responseMessage = (response) => 
+   async (dispatch) => {
+        console.log(response);
 
+    };
+    const errorMessage = (error) => {
+        console.log(error);
+    };
   return (
+    <GoogleOAuthProvider clientId="client-id">
     <div className="App app-container">
       {!mode && <Button onClick={() => setMode('single')}>Single User</Button>}
       {!mode && <Button onClick={() => setMode('broadcast')}> Broadcast</Button>}
       { mode === 'single' && <VideoCall />}
       { mode === 'broadcast' && <BroadcastVid />}
+      <GoogleLogin 
+      onSuccess={responseMessage} 
+      onError={errorMessage} 
+      useOneTap
+      />
 
     </div>
+     </GoogleOAuthProvider>
   );
 }
 
